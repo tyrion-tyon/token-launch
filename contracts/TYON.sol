eaf2abccaaff7e8d06dcd1481848fa132e1a6348
akshaydevh <akshaydevklm@gmail.com>
2022-11-09 18:25:18 +0530

    removed swap and liquidify

--- contracts/TYON.sol
+++ contracts/TYON.sol
@@ -824,7 +824,7 @@ contract TYON is Context, IERC20, Ownable {
         ) {
             contractTokenBalance = numTokensSellToAddToLiquidity;
             //add liquidity
-            swapAndLiquify(contractTokenBalance);
+            //swapAndLiquify(contractTokenBalance);
         }
 
         //indicates if fee should be deducted from transfer
@@ -839,61 +839,61 @@ contract TYON is Context, IERC20, Ownable {
         _tokenTransfer(from, to, amount, takeFee);
     }
 
-    function swapAndLiquify(uint256 contractTokenBalance) private lockTheSwap {
-        // split the contract balance into halves
-        uint256 half = contractTokenBalance / 2;
-        uint256 otherHalf = contractTokenBalance - half;
+    // function swapAndLiquify(uint256 contractTokenBalance) private lockTheSwap {
+    //     // split the contract balance into halves
+    //     uint256 half = contractTokenBalance / 2;
+    //     uint256 otherHalf = contractTokenBalance - half;
 
-        // capture the contract's current ETH balance.
-        // this is so that we can capture exactly the amount of ETH that the
-        // swap creates, and not make the liquidity event include any ETH that
-        // has been manually sent to the contract
-        uint256 initialBalance = address(this).balance;
+    //     // capture the contract's current ETH balance.
+    //     // this is so that we can capture exactly the amount of ETH that the
+    //     // swap creates, and not make the liquidity event include any ETH that
+    //     // has been manually sent to the contract
+    //     uint256 initialBalance = address(this).balance;
 
-        // swap tokens for ETH
-        swapTokensForEth(half); // <- this breaks the ETH -> HATE swap when swap+liquify is triggered
+    //     // swap tokens for ETH
+    //     //swapTokensForEth(half); // <- this breaks the ETH -> HATE swap when swap+liquify is triggered
 
-        // how much ETH did we just swap into?
-        uint256 newBalance = address(this).balance - initialBalance;
+    //     // how much ETH did we just swap into?
+    //     uint256 newBalance = address(this).balance - initialBalance;
 
-        // add liquidity to uniswap
-        addLiquidity(otherHalf, newBalance);
+    //     // add liquidity to uniswap
+    //     //addLiquidity(otherHalf, newBalance);
 
-        emit SwapAndLiquify(half, newBalance, otherHalf);
-    }
+    //     emit SwapAndLiquify(half, newBalance, otherHalf);
+    // }
 
-    function swapTokensForEth(uint256 tokenAmount) private {
-        // generate the uniswap pair path of token -> weth
-        address[] memory path = new address[](2);
-        path[0] = address(this);
-        path[1] = uniswapV2Router.WETH();
+    // function swapTokensForEth(uint256 tokenAmount) private {
+    //     // generate the uniswap pair path of token -> weth
+    //     address[] memory path = new address[](2);
+    //     path[0] = address(this);
+    //     path[1] = uniswapV2Router.WETH();
 
-        _approve(address(this), address(uniswapV2Router), tokenAmount);
+    //     _approve(address(this), address(uniswapV2Router), tokenAmount);
 
-        // make the swap
-        uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
-            tokenAmount,
-            0, // accept any amount of ETH
-            path,
-            address(this),
-            block.timestamp
-        );
-    }
+    //     // make the swap
+    //     uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+    //         tokenAmount,
+    //         0, // accept any amount of ETH
+    //         path,
+    //         address(this),
+    //         block.timestamp
+    //     );
+    // }
 
-    function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private {
-        // approve token transfer to cover all possible scenarios
-        _approve(address(this), address(uniswapV2Router), tokenAmount);
+    // function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private {
+    //     // approve token transfer to cover all possible scenarios
+    //     _approve(address(this), address(uniswapV2Router), tokenAmount);
 
-        // add the liquidity
-        uniswapV2Router.addLiquidityETH{value: ethAmount}(
-            address(this),
-            tokenAmount,
-            0, // slippage is unavoidable
-            0, // slippage is unavoidable
-            owner(),
-            block.timestamp
-        );
-    }
+    //     // add the liquidity
+    //     uniswapV2Router.addLiquidityETH{value: ethAmount}(
+    //         address(this),
+    //         tokenAmount,
+    //         0, // slippage is unavoidable
+    //         0, // slippage is unavoidable
+    //         owner(),
+    //         block.timestamp
+    //     );
+    // }
 
     //this method is responsible for taking all fee, if takeFee is true
     function _tokenTransfer(
