982697e5e71f0b64c5030a2f5ac6bb6395f58cf9
akshaydevh <akshaydevklm@gmail.com>
2022-12-02 00:35:02 +0530

    wallet config added

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports.TYON_V1_CONFIG = {
+    _growthX: "0xFBd8a18B35595c148c0A6fA1D0c86Adf8413cbc4",
+    _tyonShield: "0xb4BadcA1a242f59557baB3581A42bc314eCc4179",
+    _fundMe:"0x5f2BE4fd9130d5940df7B8170f44e05f32de9877" ,
+    _ecosystemGrowth: "0x699312b02236b354f4C59D293aF9b130ac556567"
+};
