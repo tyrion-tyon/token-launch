cf1391e6429df28ea3655c9ceb877e038c2ea094
akshaydevh <akshaydevklm@gmail.com>
2023-03-15 14:24:23 +0530

    tytonShield name change

--- migrations/2_TYON_migration.js
+++ migrations/2_TYON_migration.js
@@ -7,7 +7,7 @@ module.exports = async function (deployer) {
     TYON_V1,
     [
       TYON_V1_CONFIG._growthX,
-      TYON_V1_CONFIG._tyonShield,
+      TYON_V1_CONFIG._tyrionShield,
       TYON_V1_CONFIG._fundMe,
       TYON_V1_CONFIG._ecosystemGrowth,
       TYON_V1_CONFIG._growthxWallet
