4a297753aef4fefaf21f68483be5a8791741186e
akshaydevh <akshaydevklm@gmail.com>
2023-03-23 21:11:05 +0530

    deploy according to network

--- migrations/2_TYON_migration.js
+++ migrations/2_TYON_migration.js
@@ -2,23 +2,42 @@ const { deployProxy, upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 const TYON_V1 = artifacts.require("TYON_V1");
 const { TYON_V1_CONFIG } = require("../config/config");
 
-module.exports = async function (deployer) {
-  const instance = await deployProxy(
-    TYON_V1,
-    [
-      TYON_V1_CONFIG._growthX,
-      TYON_V1_CONFIG._tyrionShield,
-      TYON_V1_CONFIG._fundMe,
-      TYON_V1_CONFIG._ecosystemGrowth,
-      TYON_V1_CONFIG._growthxWallet
-    ],
-    { deployer }
-  );
-//   await instance.transferOwnership(
-//     "0xcF872915E9187Ef676d8fFA83f9bc4E46b0293Cf"
-//   );
-//   const owner = await instance.owner();
-//   console.log("new owner:", owner);
+module.exports = async function (deployer, network, accounts) {
+  let instance;
+  console.log(network);
+  if (network == "binanceTestnet" || network == "binance") {
+    instance = await deployProxy(
+      TYON_V1,
+      [
+        accounts[1],
+        accounts[2],
+        accounts[3],
+        accounts[4],
+        accounts[5],
+        accounts[6],
+      ],
+      { deployer }
+    );
+  } else {
+    instance = await deployProxy(
+      TYON_V1,
+      [
+        TYON_V1_CONFIG._growthX,
+        TYON_V1_CONFIG._tyrionShield,
+        TYON_V1_CONFIG._fundMe,
+        TYON_V1_CONFIG._ecosystemGrowth,
+        TYON_V1_CONFIG._growthxWallet,
+        TYON_V1_CONFIG._tyrionShieldWallet,
+      ],
+      { deployer }
+    );
+  }
+
+  //   await instance.transferOwnership(
+  //     "0xcF872915E9187Ef676d8fFA83f9bc4E46b0293Cf"
+  //   );
+  //   const owner = await instance.owner();
+  //   console.log("new owner:", owner);
 };
 
 // module.exports = async function (deployer) {
