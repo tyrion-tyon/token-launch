2d7fb728bc1608444924b0be8ce9fef41d44f5a0
akshaydevh <akshaydevklm@gmail.com>
2023-04-23 12:14:26 +0530

    deployment cofig updated

--- migrations/2_TYON_migration.js
+++ migrations/2_TYON_migration.js
@@ -4,34 +4,19 @@ const { TYON_V1_CONFIG } = require("../config/config");
 
 module.exports = async function (deployer, network, accounts) {
   let instance;
-  console.log(network);
-  if (network == "binanceTestnet" || network == "binance") {
-    instance = await deployProxy(
-      TYON_V1,
-      [
-        accounts[1],
-        accounts[2],
-        accounts[3],
-        accounts[4],
-        accounts[5],
-        accounts[6],
-      ],
-      { deployer }
-    );
-  } else {
-    instance = await deployProxy(
-      TYON_V1,
-      [
-        TYON_V1_CONFIG._growthX,
-        TYON_V1_CONFIG._tyrionShield,
-        TYON_V1_CONFIG._fundMe,
-        TYON_V1_CONFIG._ecosystemGrowth,
-        TYON_V1_CONFIG._growthxWallet,
-        TYON_V1_CONFIG._tyrionShieldWallet,
-      ],
-      { deployer }
-    );
-  }
+
+  instance = await deployProxy(
+    TYON_V1,
+    [
+      TYON_V1_CONFIG._growthX,
+      TYON_V1_CONFIG._tyrionShield,
+      TYON_V1_CONFIG._fundMe,
+      TYON_V1_CONFIG._ecosystemGrowth,
+      TYON_V1_CONFIG._growthxWallet,
+      TYON_V1_CONFIG._tyrionShieldWallet,
+    ],
+    { deployer }
+  );
 
   //   await instance.transferOwnership(
   //     "0xcF872915E9187Ef676d8fFA83f9bc4E46b0293Cf"
